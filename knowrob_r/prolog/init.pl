113b248b78daa97c2454263d3426b31a1e98a593
Lisset Salinas <salinas@uni-bremen.de>
2018-06-07 23:11:22 +0200

    Knowrob r added

---
+++ knowrob_r/prolog/init.pl
@@ -0,0 +1,9 @@
+:- register_ros_package(knowrob_common).
+:- register_ros_package(knowrob_mongo).
+:- register_ros_package(knowrob_r).
+
+:- use_module(library('real')).
+:- use_module(library('pack')).
+:- use_module(library('plttex')).
+
+
